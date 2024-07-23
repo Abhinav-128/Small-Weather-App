68b8cbecc691b1e758556c1620d3d60088f1259d
Abhinav <goswami12897@gmail.com>
2024-07-23 22:38:29 +0530

    Add files via upload

--- Weather_Report.py
+++ Weather_Report.py
@@ -15,14 +15,10 @@ def WeatherUpdate():
            humdity = weather_json.get('current').get('humidity')
            aqi = weather_json.get('current').get('air_quality')
            currentTime = weather_json.get('location').get('localtime')
-           print("Current weather in",city,"of", country,"of Region",region,"at",currentTime,"is ",description, "and the temprature is ",temp,"C and feels like",tempFeel,f"C, with humidity {humdity}% and Air Conditions are",aqi)
+           print("Current weather in",city,"of Region",region,"of", country,"at",currentTime,"is ",description, "and the temprature is ",temp,"C and feels like",tempFeel,f"C, with humidity {humdity}% and Air Conditions are",aqi)
            print("")
-        except requests.exceptions.HTTPError:
-              print(f"Error: The city '{city}' was not found in the weather database.")
-        except requests.exceptions.RequestException as req_err:
-              print(f"Network error occurred: {req_err}")
-        except Exception as err:
-              print(f"Error: The city '{city}' was not found in the weather database.{err}")
+        except:
+              print(f"Error: The city '{city}' was not found in the weather database")
 
 def main(): 
     user_Choice = "yes"
